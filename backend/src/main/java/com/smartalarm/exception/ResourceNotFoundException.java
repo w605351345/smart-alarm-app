6f88430825f2acf187bd4f050fca74aad5babd30
w605351345 <47136579+w605351345@users.noreply.github.com>
2026-03-20 16:58:43 +0800

    feat: add ResourceNotFoundException.java

---
+++ backend/src/main/java/com/smartalarm/exception/ResourceNotFoundException.java
@@ -0,0 +1,27 @@
+package com.smartalarm.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResourceNotFoundException extends RuntimeException {
+    private String resourceName;
+    private String fieldName;
+    private Object fieldValue;
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
+        super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
+    }
+}
